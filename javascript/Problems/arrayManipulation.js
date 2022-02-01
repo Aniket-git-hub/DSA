554a533e30073b9f37ec76416c7a6bc17b785e1a
Aniket Singh <singhdharmvir81@gmail.com>
2022-02-01 10:58:42 +0530

    changes in readme

--- javascript/Problems/arrayManipulation.js
+++ javascript/Problems/arrayManipulation.js
@@ -8,6 +8,19 @@
  *  2. 2D_INTEGER_ARRAY queries
  */
 function arrayManipulation(n, queries) {
-    // Write your code here
+    let arr = new Array(n + 1).fill(0);
+    let max = 0;
+    for (let i = 0; i < queries.length; i++) {
+        let [a, b, k] = queries[i];
+        arr[a - 1] += k;
+        if (b <= n)
+            arr[b] -= k;
+    }
+    for (let i = 1; i < arr.length; i++) {
+        arr[i] += arr[i - 1];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
 }
 console.log(arrayManipulation(5, [[1, 2, 100], [2, 5, 100], [3, 4, 100]])); // 200
