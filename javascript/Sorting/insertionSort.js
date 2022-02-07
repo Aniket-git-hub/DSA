43d06a7ffeec177f3d0d6e5cbb29933660dd19e1
Aniket Singh <singhdharmvir81@gmail.com>
2022-02-07 11:24:54 +0530

    Merge branch 'Algorithms' of https://github.com/Aniket-git-hub/DSA into Algorithms

---
+++ javascript/Sorting/insertionSort.js
@@ -0,0 +1,20 @@
+"use strict";
+// insertion sort Algorihtm
+function insertionSort(a) {
+    for (let i = 0; i <= a.length - 1; i++) {
+        let key = i;
+        while (key > 0 && a[key] < a[key - 1]) {
+            let temp = a[key];
+            a[key] = a[key - 1];
+            a[key - 1] = temp;
+            key--;
+        }
+    }
+    return a;
+}
+let a = [12, 11, 13, 5, 6];
+console.log("Insertion Sort");
+console.log(`Given array:
+ [ ${a} ]`);
+console.log(`Sorted Array: 
+[ ${insertionSort(a)} ]`);
