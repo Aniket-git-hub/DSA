8fcda0379b0797984180653d629defa4d0ac125d
Aniket Singh <singhdharmvir81@gmail.com>
2023-01-07 17:57:56 +0530

    Added: route between two nodes algorithm

---
+++ graphs/routeBetweenNodes.js
@@ -0,0 +1,46 @@
+// Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+const testG = {
+    'A': ['B', 'C'],
+    'B': ['D', 'F', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+        
+
+// iterative implementation of breadth-first search
+
+const path = (graph, start, end) => {
+    if (start == end) return true
+    
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+    while (queue.length) {
+        const node = queue.shift();
+        const neighbors = graph[node];
+        
+        for (let i = 0; i < neighbors.length; i++) {
+            if (!visited[neighbors[i]]) {
+                if (neighbors[i] == end) { 
+                    return true
+                } else {
+                    visited[neighbors[i]] = true;
+                    queue.push(neighbors[i]);
+                }
+            }
+        }
+    }
+    return false
+}
+
+
+console.log(path(testG, 'A', 'B'))
+console.log(path(testG, 'A', 'C'))
+console.log(path(testG, 'A', 'D'))
+console.log(path(testG, 'A', 'E'))
+console.log(path(testG, 'A', 'F'))
+console.log(path(testG, 'F', 'A'))
+console.log(path(testG, 'E', 'B'))
+console.log(path(testG, 'C', 'A'))
